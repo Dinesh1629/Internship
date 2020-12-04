5586b3955ca6e5a7408f2f23ba3793a13e77209f
Dinesh1629 <42836590+Dinesh1629@users.noreply.github.com>
2020-12-04 16:32:13 +0530

    Add files via upload

---
+++ Let-Var/example.js
@@ -0,0 +1,23 @@
+var developer = {
+    title: "Developer",
+    department: "I.T.",
+    location: "Building 3, 2nd Floor"
+  };
+  
+  var techLeadTitle = {title: "Tech Lead"};
+  
+  var techLead = {...developer};
+  
+  console.log(techLeadTitle);
+  // {
+  //   title: "Tech Lead",
+  //   department: "I.T.",
+  //   location: "Building 3, 2nd Floor"
+  //};
+  
+  console.log(developer);
+  // {
+  //   title: "Developer",
+  //   department: "I.T.",
+  //   location: "Building 3, 2nd Floor"
+  // };
